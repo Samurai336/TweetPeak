2616a4ae7e973ee3fea3b03fb96d53eedd5cb79a
Samurai336 <Samurai336@gmail.com>
2012-03-30 01:26:21 -0400

    Added windows folder

---
+++ windows/DLLApplication/RestApi/RestApi.cpp
@@ -0,0 +1,14 @@
+// This is the main DLL file.
+
+
+#include "RestApi.h"
+
+
+
+/*
+void RestApi::poop::ReturnAPoop()
+{
+	cout<<"Poop"<<endl; 
+	
+}
+*/
---
+++ windows/DLLApplication/RestApi/RestApi.h
@@ -0,0 +1,25 @@
+// RestApi.h
+
+#pragma once
+#include <iostream> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <curl\curl.h> 
+
+
+using namespace std;
+
+namespace RestApi
+{
+
+	class poop
+	{
+		public:
+			//see # 4 in notes 
+			//__declspec(dllimport) void ReturnAPoop(); 
+			
+
+
+
+	}; 
+}
